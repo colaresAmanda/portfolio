92c2102a86a272e5c613ed4f3fd89962f3f599ae
colaresAmanda <colares.amd@gmail.com>
2023-07-17 10:21:40 -0300

    header is responsive and with dark theme

--- script.js
+++ script.js
@@ -1,30 +1,56 @@
-function menuSettings() {
-    // Code related to menu settings
-}
+// Function to handle theme-related actions
+function themeActions() {
+    const toggleThemeCheckbox = document.querySelector('#toggle-checkbox');
 
-function themeSettings(toggleThemeCheckbox) {
-    const darkTheme = toggleThemeCheckbox.checked;
-
-    if (darkTheme) {
+    // Event listener for theme toggle checkbox
+    toggleThemeCheckbox.addEventListener('click', () => {
+        const lightTheme = toggleThemeCheckbox.checked; // Retrieve the current checkbox state
         const body = document.querySelector('body');
-        body.classList.toggle('dark');
+
+        // Check if dark theme is enabled
+        if (lightTheme) {
+            body.classList.add('dark'); // Add 'dark' class to body element
+        } else {
+            body.classList.remove('dark'); // Remove 'dark' class from body element
+        }
+
+        // Store the state of the theme toggle checkbox in local storage
+        localStorage.setItem('toggleThemeState', toggleThemeCheckbox.checked);
+    });
+
+    // Check if the theme toggle checkbox state is stored in local storage
+    if (localStorage.getItem('toggleThemeState')) {
+        const isChecked = JSON.parse(localStorage.getItem('toggleThemeState'));
+        toggleThemeCheckbox.checked = isChecked;
+        const body = document.querySelector('body');
+
+        // Apply the stored theme state to the body element
+        if (!isChecked) {
+            body.classList.add('dark'); // Add 'dark' class to body element
+        } else {
+            body.classList.remove('dark'); // Remove 'dark' class from body element
+        }
     }
 }
 
-function init() {
-    //Toggle menu
-    const hamburguerIconCheckbox = document.querySelector('#hamburguer-icon-checkbox');
+// Function to handle menu-related actions
+function menuActions() {
+    // Toggle menu
+    const hamburgerIconCheckbox = document.querySelector('#hamburger-icon-checkbox');
     const menu = document.querySelector('.menu');
 
-    hamburguerIconCheckbox.addEventListener('click', () => {
-        menu.classList.toggle('visible');
+    // Event listener for menu toggle checkbox
+    hamburgerIconCheckbox.addEventListener('change', (e) => {
+        menu.classList.toggle('visible'); // Toggle 'visible' class on menu element
     });
 
+    // Highlight current menu section
+    const menuItems = document.querySelectorAll('.menu-item:not(:last-child)');
 
-    const menuItems = document.querySelectorAll('.menu-item');
-
+    // Event listener for each menu item
     menuItems.forEach((menuItem) => {
         menuItem.addEventListener('click', () => {
+            // Hide menu
             menu.classList.toggle('visible');
 
             // Remove 'current' class from all menu items
@@ -36,10 +62,22 @@ function init() {
             menuItem.classList.add('current');
         });
     });
-
-    // Toggle theme
-    const toggleThemeCheckbox = document.querySelector('#toggle-checkbox');
-    toggleThemeCheckbox.addEventListener('click', () => themeSettings(toggleThemeCheckbox));
 }
 
-init();
+function uncheckCheckboxes() {
+    // Get the checkbox elements
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+    // Uncheck the checkboxes explicitly
+    checkboxes.forEach(checkbox => {
+        checkbox.checked = false;
+    });
+}
+
+function init() {
+    menuActions(); // Call menuActions function to handle menu-related actions
+    themeActions(); // Call themeActions function to handle theme-related actions
+    uncheckCheckboxes(); // Call uncheckCheckboxes function to reset checkbox state
+}
+
+init(); // Call init function to initialize the menu, theme actions, and reset checkboxes
