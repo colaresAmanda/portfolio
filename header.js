2e2928c69216c1906c20dc3b1469542d21c655f5
colaresAmanda <colares.amd@gmail.com>
2023-07-31 10:32:14 -0300

    _

--- header.js
+++ header.js
@@ -5,8 +5,8 @@ function themeActions() {
 
     // Event listener for theme toggle checkbox
     toggleThemeCheckbox.addEventListener('click', () => {
-        body.classList.toggle('dark'); 
-    });   
+        body.classList.toggle('dark');
+    });
 }
 
 // Function to handle menu-related actions
@@ -25,10 +25,41 @@ function menuActions() {
 
     // Event listener for each menu item
     menuItems.forEach((menuItem) => {
-        menuItem.addEventListener('click', () => {
+        menuItem.addEventListener('click', (e) => {
             // Hide menu
+            e.preventDefault(); // Prevent default anchor tag behavior
+
             menu.classList.toggle('visible');
 
+            // console.log(menuItem, window.innerHeight)
+
+
+            switch (menuItem.textContent) {
+                case "Início":
+                    window.scrollTo(0, window.sectionInitialY[0])
+
+
+                    break;
+                case "O que faço":
+                    window.scrollTo(0, window.sectionInitialY[1])
+
+                    break;
+                case "Sobre mim":
+                    window.scrollTo(0, window.sectionInitialY[2])
+                    break;
+                case "Educação":
+                    window.scrollTo(0, window.sectionInitialY[3])
+                    break;
+                case "Projetos":
+                    window.scrollTo(0, window.sectionInitialY[4])
+                    break;
+                    case "Contato":
+                    window.scrollTo(0, window.sectionInitialY[5])
+                    break;
+                default:
+                    window.scrollTo(0, 0)
+                    break;
+            }
             // Remove 'current' class from all menu items
             menuItems.forEach((item) => {
                 item.classList.remove('current');
--- main.js
+++ main.js
@@ -17,8 +17,77 @@ function rotateHeroIcons() {
     }
 }
 
+function scrollAnimation() {
+    const sections = document.querySelectorAll('section')
+    const triggerBottom = window.innerHeight / 5 * 4
+
+    const sectionInitialY = []
+
+    sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top
+
+        if (sectionTop < triggerBottom) {
+            section.classList.add("visible")
+        }
+        else if (sectionTop > triggerBottom) {
+            section.classList.remove("visible")
+        }
+
+
+        const sectionRect = section.getBoundingClientRect();
+        const offsetTop = sectionRect.top + window.pageYOffset;
+        sectionInitialY.push(offsetTop)
+    })
+
+    window.sectionInitialY = sectionInitialY
+
+}
+
+// function toggleEducationSection() {
+//     const educationSection = document.querySelector('.education');
+
+//     educationSection.addEventListener('click', (event) => {
+//         if (event.target.classList.contains('education-title')) {
+//             const educationTitles = educationSection.querySelectorAll('.education-title');
+
+//             educationTitles.forEach((educationTitle) => {
+//                 educationTitle.classList.remove('selected');
+//             });
+
+//             event.target.classList.toggle('selected');
+//         }
+//     });
+// }
+
+function toggleEducationSection() {
+    const certificationsButton = document.querySelector('#education-title-certification')
+    const educationButton = document.querySelector('#education-title-education')
+
+    const educationContainer = document.querySelector('.education-container')
+
+    certificationsButton.addEventListener('click', () => {
+        educationContainer.classList.add('toggle')
+
+
+        certificationsButton.classList.toggle('selected');
+        educationButton.classList.toggle('selected')
+    })
+    educationButton.addEventListener('click', () => {
+        educationContainer.classList.remove('toggle')
+
+        educationButton.classList.toggle('selected')
+        certificationsButton.classList.toggle('selected');
+
+    })
+}
+
 function init() {
     rotateHeroIcons()
+
+    scrollAnimation()
+    window.addEventListener('scroll', scrollAnimation)
+
+    toggleEducationSection()
 }
 
 init()
